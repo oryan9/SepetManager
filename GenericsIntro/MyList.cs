1a6397298b7393391b30d2f1b6e8f15091407699
Öryan KARAKAŞ <karakasoryan@gmail.com>
2021-10-18 14:38:41 +0300

    20 ocak ders

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T [] İtems;
+        public MyList()
+        {
+            İtems = new T[0];
+
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = İtems;
+            İtems = new T[İtems.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                İtems[i] = tempArray[i];
+            }
+
+            İtems[İtems.Length - 1] = item;
+        }
+    }
+}
---
+++ Koleksiyonlar1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "ÖMER", "HALİL" };
+
+            //Console.WriteLine(isimler[0]);
+
+            //isimler = new string[5];
+            //isimler[4] = "il";
+
+            List<string> isimler2 = new List<string> { "Engin", " Ömer", "Faruk" };
+
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[3]);
+
+
+
+            
+
+
+
+
+        }
+    }
+}
